fef2ab0a3d28e412da5cd5bbea21285dee75abb3
nfridoy <nfridoy@gmail.com>
2026-09-23 14:21:24 +0600

    ✨ route: book

---
+++ routes/routes.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"go-fiber-crud/handlers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupRoutes(app *fiber.App) {
+	api := app.Group("/api/v1")
+
+	// Book endpoints
+	books := api.Group("/books")
+	books.Post("/", handlers.CreateBook)
+	books.Get("/", handlers.GetBooks)
+	books.Get("/:id", handlers.GetBookByID)
+	books.Put("/:id", handlers.UpdateBook)
+	books.Delete("/:id", handlers.DeleteBook)
+}
